e17fa8c3c9d4a90c15ff8586a06b3f7d5d1be093
高楚鈞 <T10822758@live.yuntech.edu.tw>
2023-08-01 16:43:20 +0800

    使用React的生命週期函式

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 
 class Footer extends Component {
     state = {
-        name: "Ryan"
+        // name: "Ryan"
+        name: ""
     }
 
     createAlert() {
@@ -15,6 +16,10 @@ class Footer extends Component {
         this.setState({ name: event.target.value });
         // 這裡的狀態並不會即時更新，仍會是先前的狀態
         console.log(this.state.name);
+    }
+
+    componentDidMount() {
+        this.setState({ name: "Ryan" });
     }
 
     render() {
