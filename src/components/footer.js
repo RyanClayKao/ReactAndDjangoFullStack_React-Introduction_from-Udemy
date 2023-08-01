6cc39f8629e496b3b538cc3598d2f6b93501cc00
高楚鈞 <T10822758@live.yuntech.edu.tw>
2023-08-01 12:00:12 +0800

    練習React的State基礎使用

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
-class Footer extends Component{
-    createAlert(){
+class Footer extends Component {
+    state = {
+        name: "Ryan"
+    }
+
+    createAlert() {
         alert("Hello, you clicked me!")
     }
 
-    inputChanged(){
+    inputChanged = (event) => {
         console.log("changed");
+        // console.log(this.state.name);
+        this.setState({ name: event.target.value });
+        // 這裡的狀態並不會即時更新，仍會是先前的狀態
+        console.log(this.state.name);
     }
 
     render() {
@@ -15,7 +23,9 @@ class Footer extends Component{
                 {/* <h3 onClick={this.createAlert}>This is footer component(class component)</h3> */}
                 <h3 onClick={this.props.appAlert}>This is footer component(class component)</h3>
                 <p>trademark: {this.props.trademark}</p>
-                <input onChange={this.inputChanged} type="text"/>
+                <input
+                    value={this.state.name}
+                    onChange={this.inputChanged} type="text" />
             </React.Fragment>
         )
     }
