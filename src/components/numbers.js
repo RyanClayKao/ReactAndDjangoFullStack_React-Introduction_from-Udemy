a80fcefc2293b36a9455df89a232d4fe85a921d9
RyanClayKao <f256987584@gmail.com>
2023-08-02 16:27:44 +0800

    學習使用React Hooks的 useEffect

--- src/components/numbers.js
+++ src/components/numbers.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Numbers = () => {
     const [numbers, setNumbers] = useState(['one', 'two', 'three']);
@@ -8,13 +8,27 @@ const Numbers = () => {
         setNumbers([...numbers, 'four'])
     }
 
+    const addLetter = () => {
+        setLetters([...letters, 'd'])
+    }
+
+    // 當只有空陣列的時候，就相當於只有 Mount的時候會被觸發
+    // 而若有加入其他 useState的 state變數到陣列中時，當有state發生改變就會被觸發
+    useEffect( () => {
+        console.log("triggered");
+    }, [numbers, letters])
+
     return (
         <div>
             <h1>Numbers</h1>
-            {numbers.map(num => {
-                return <h4 key={num}>{num}</h4>
+            {numbers.map((num, index) => {
+                return <h4 key={index}>{num}</h4>
             })}
             <button onClick={addNumber}>Add a number</button>
+            {letters.map((letter, index) => {
+                return <h4 key={index}>{letter}</h4>
+            })}
+            <button onClick={addLetter}>Add a letter</button>
         </div>
     )
 }
